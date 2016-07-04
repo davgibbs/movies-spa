ef264e19b5f2428b17b3408d339b9792bf4a5286
David Gibbons <dgibbons999@gmail.com>
2016-07-04 01:36:51 +0100

    More Progress

--- apps/movies/models.py
+++ apps/movies/models.py
@@ -3,5 +3,30 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Movie(models.Model):
+    """ Movie Information """
+    MOVIE_GENRES = (
+        ('action', 'Action'),
+        ('adventure', 'Adventure'),
+        ('comedy', 'Comedy'),
+        ('crime_gangster', 'Crime & Gangster'),
+        ('drama', 'Drama'),
+        ('historical', 'Historical'),
+        ('horror', 'Horror'),
+        ('musicals', 'Musicals'),
+        ('science_fiction', 'Science Fiction'),
+        ('war', 'War'),
+        ('westerns', 'Westerns'),
+    )
 
+    title = models.CharField(max_length=100, unique=True)
+    release_year = models.IntegerField(max_length=4)
+    director = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100, choices=MOVIE_GENRES)
 
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Movie'
+        verbose_name_plural = 'Movies'
--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import MovieSerializer
+from .models import Movie
+
+
+def index(request):
+    return render(request, "movies/index.html", {})
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows movie to be viewed or edited.
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer

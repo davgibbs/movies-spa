c8dfaac7e11f01eb29d6b7c89f6097dcd9c55896
David Gibbons <dgibbons999@gmail.com>
2016-07-20 09:40:25 +0100

    improve portability of test

--- apps/movies/static/movies/test/controllers_tests.js
+++ apps/movies/static/movies/test/controllers_tests.js
@@ -1,31 +1,30 @@
-//(function() {
+(function() {
 
+    var $controller, $location;
 
-var $controller, $location;
+    module("NavigationCtrl Tests", {
+        setup: function() {
+            var injector = angular.injector([ 'ng', 'movieApp' ]);
+            this.$scope = injector.get('$rootScope').$new();
 
-module("NavigationCtrl Tests", {
-    setup: function() {
-        var injector = angular.injector([ 'ng', 'movieApp' ]);
-        scope = injector.get('$rootScope').$new();
+            $location = {
+                path: function() {return '/movies/1';}
+            };
 
-        $location = {
-            path: function() {return '/movies/1';}
-        };
+            $controller = injector.get('$controller');
+            $controller('NavigationCtrl', {
+                $scope : this.$scope,
+                $location: $location
+            });
+        }
+    });
 
-        controller = injector.get('$controller');
-        controller('NavigationCtrl', {
-            $scope : scope,
-            $location: $location
-        });
-    }
-});
+    test("NavigationCtrl", function() {
+        current_page = this.$scope.isCurrentPath('/about');
+        strictEqual(current_page, false, "check return false for active");
 
-test("NavigationCtrl", function() {
-    current_page = scope.isCurrentPath('/about');
-    strictEqual(current_page, false, "check return false for active");
+        current_page = this.$scope.isCurrentPath('/movies');
+        strictEqual(current_page, true, "check return true for movies");
+    });
 
-    current_page = scope.isCurrentPath('/movies');
-    strictEqual(current_page, true, "check return true for movies");
-});
-
-//})();
+})();

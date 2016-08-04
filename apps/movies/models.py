61d64c237ba601c211a010b3578d2814ed3a63d7
David Gibbons <david.gibbons@ammeon.com>
2016-08-04 12:03:23 +0100

    Build work

--- apps/movies/models.py
+++ apps/movies/models.py
@@ -27,7 +27,6 @@ class Movie(models.Model):
     image = models.ImageField(upload_to='movies/', default='movies/Movie.jpg')
     rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)], default=3)
 
-
     def __unicode__(self):
         return self.title
 

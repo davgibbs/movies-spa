56e6a42eb2c802478c9ddad1ecb67c466be18403
David Gibbons <dgibbons999@gmail.com>
2016-08-10 00:05:23 +0100

    beautify html js and css

--- apps/movies/static/movies/js/controllers.js
+++ apps/movies/static/movies/js/controllers.js
@@ -51,7 +51,7 @@ angular.module('movieApp.controllers', ['angularUtils.directives.dirPagination']
 
 }).controller('MovieViewController', function($scope, $stateParams, $http) {
 
-    $http.get("/api/movies/" + $stateParams.id )
+    $http.get("/api/movies/" + $stateParams.id)
         .then(function successCallback(response) {
             $scope.movie = response.data;
         });
@@ -149,7 +149,7 @@ angular.module('movieApp.controllers', ['angularUtils.directives.dirPagination']
         });
 
     $scope.loadMovie = function() {
-        $http.get("/api/movies/" + $stateParams.id )
+        $http.get("/api/movies/" + $stateParams.id)
             .then(function successCallback(response) {
                 $scope.movie = response.data;
             });

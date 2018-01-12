a24b124bfa82e82b8cfb44e0516aee76e7ab17fe
David Gibbons <dgibbons999@gmail.com>
2018-01-12 16:38:12 +0000

    Another test

--- apps/movies/tests.py
+++ apps/movies/tests.py
@@ -14,6 +14,20 @@ class MovieTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, [])
 
+    def test_list_movies_multiple(self):
+        movie_genre = MovieGenre(name='Comedy')
+        movie_genre.save()
+
+        movie = Movie(title="Movie 1")
+        movie.save()
+
+        client = APIClient()
+        response = client.get('/api/movies', {}, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [OrderedDict([('id', 1),('title', 'Movie 1'), ('summary', ''),
+            ('release_year', 2016), ('director', ''), ('genre', 1), ('genre_obj', OrderedDict([('id', 1),
+            ('name', 'Comedy')])), ('image', 'http://testserver/media/movies/Movie.jpg'), ('rating', 3)])])
+
 
 class MovieGenreTestCase(TestCase):
 

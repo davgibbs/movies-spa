ae1a5a5d4fcca2850018bc2ee89eec0d625f54c1
David Gibbons <dgibbons999@gmail.com>
2018-01-12 15:29:00 +0000

    Added Python tests

--- apps/movies/tests.py
+++ apps/movies/tests.py
@@ -1,3 +1,7 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+
+class MovieTestCase(TestCase):
+
+    def test_this_true(self):
+        self.assertEqual(True, True)

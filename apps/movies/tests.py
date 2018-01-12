8144690635ecbf959b5a035f229ac80601be84bb
David Gibbons <dgibbons999@gmail.com>
2018-01-12 16:15:39 +0000

    Added another test

--- apps/movies/tests.py
+++ apps/movies/tests.py
@@ -1,5 +1,9 @@
+from collections import OrderedDict
+
 from django.test import TestCase
 from rest_framework.test import APIClient
+
+from movies.models import Movie, MovieGenre
 
 
 class MovieTestCase(TestCase):
@@ -9,3 +13,16 @@ class MovieTestCase(TestCase):
         response = client.get('/api/movies', {}, format='json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, [])
+
+
+class MovieGenreTestCase(TestCase):
+
+    def test_list_genres(self):
+        movie_genre = MovieGenre(name='Comedy')
+        movie_genre.save()
+
+        client = APIClient()
+        response = client.get('/api/movies-genres', {}, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [OrderedDict([('id', 1), ('name', 'Comedy')])])
+        movie_genre.delete()

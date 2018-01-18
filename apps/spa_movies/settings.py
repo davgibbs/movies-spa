ed25e5baa9d5d222c8a12fb2689ede1ec889fd82
David Gibbons <dgibbons999@gmail.com>
2018-01-18 10:51:18 +0000

    fix build

--- apps/spa_movies/settings.py
+++ apps/spa_movies/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': []
     #    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # ]
 }

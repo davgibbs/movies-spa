c9a25d2bfcfaeca8b941942a33a24c2b7f9bcf73
David Gibbons <dgibbons999@gmail.com>
2018-05-09 00:39:09 +0100

    More tests

--- apps/movies/static/movies/test/spec/ServicesSpec.js
+++ apps/movies/static/movies/test/spec/ServicesSpec.js
@@ -11,9 +11,9 @@ describe('PopupService Tests', function(){
                 windowmock.confirm = function(){return true;};
                 return windowmock;
             });
-            }
+        }
 
-        ));
+    ));
 
     var popupService;
 
@@ -26,6 +26,67 @@ describe('PopupService Tests', function(){
         expect(angular.isFunction(popupService.showPopup)).toBe(true);
         var result = popupService.showPopup('hello');
         expect(result).toBe(true);
+
+    });
+
+});
+
+describe('Session Tests', function(){
+
+    beforeEach(angular.mock.module('movieApp.services'));
+
+    var Session;
+
+    beforeEach(angular.mock.inject(function (_Session_) {
+        Session = _Session_;
+    }));
+
+    it('session true', function(){
+
+        expect(angular.isFunction(Session.create)).toBe(true);
+        Session.create('sessionId', 'userid', 'username');
+        expect(Session.userId).toBe('userid');
+        expect(Session.userName).toBe('username');
+
+        Session.destroy();
+        expect(Session.userId).toBe(null);
+        expect(Session.userName).toBe(null);
+
+    });
+
+});
+
+describe('AuthService Tests', function(){
+
+    beforeEach(angular.mock.module('movieApp.services',
+        // mock the window dependency
+        function ($provide) {
+            $provide.factory('Session', function () {
+                var sessionmock = jasmine.createSpy('Session');
+                sessionmock.userName = 'test-user';
+                sessionmock.userId = 'test-id';
+                return sessionmock;
+            });
+        }
+    ));
+
+    var AuthService;
+
+    beforeEach(angular.mock.inject(function (_AuthService_, Session) {
+        AuthService = _AuthService_;
+    }));
+
+    it('is authenticated', function(){
+
+        expect(angular.isFunction(AuthService.isAuthenticated)).toBe(true);
+        var authenticated = AuthService.isAuthenticated();
+        expect(authenticated).toBe(true);
+
+        var username = AuthService.username();
+        expect(username).toBe('test-user');
+
+        var userid = AuthService.userId();
+        expect(userid).toBe('test-id');
 
     });
 
